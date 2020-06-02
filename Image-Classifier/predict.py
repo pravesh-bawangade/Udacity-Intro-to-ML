d2742f2fa0272155e14f867fc23b335dc165689e
Pravesh Bawangade <pptechno52@gmail.com>
2020-06-03 00:07:06 +0530

    updated classifier

--- Image-Classifier/predict.py
+++ Image-Classifier/predict.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import numpy as np
@@ -28,7 +29,7 @@ def predict(image_path, model, top_k):
     top_values, top_indices = tf.math.top_k(prediction, top_k)
     print("These are the top propabilities", top_values.numpy()[0])
 
-    top_classes = [class_names[str(value)] for value in top_indices.cpu().numpy()[0]]
+    top_classes = [class_names[str(value + 1)] for value in top_indices.cpu().numpy()[0]]
     print('Of these top classes', top_classes)
     return top_values.numpy()[0], top_classes
 
@@ -37,7 +38,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Udacity Image Classifier")
     parser.add_argument('--model_path', nargs='?', default='./1591029114.h5',
                         help='Model Path')
-    parser.add_argument('--img_path', nargs='?', default='./test_images/orange_dahlia.jpg',
+    parser.add_argument('--img_path', nargs='?', default='./test_images/wild_pansy.jpg',
                         help='Image Location')
     parser.add_argument('--top_k', nargs='?', default=5,
                         help='Return the top K most likely classes')
